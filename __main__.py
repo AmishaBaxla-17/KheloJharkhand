6323bdceaa9f04116b60cc75084cf378d1c859aa
AmishaBaxla-17 <92683278+AmishaBaxla-17@users.noreply.github.com>
2022-07-15 20:46:53 +0530

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,3 +1,12 @@
-from .features import pilinfo
+import argparse
 
-pilinfo()
+from pip._vendor.certifi import contents, where
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--contents", action="store_true")
+args = parser.parse_args()
+
+if args.contents:
+    print(contents())
+else:
+    print(where())
