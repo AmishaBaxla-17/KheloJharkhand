f0b46fba17012d6cad19a5f4b72b1b39e641f557
AmishaBaxla-17 <92683278+AmishaBaxla-17@users.noreply.github.com>
2022-07-15 20:32:18 +0530

    Add files via upload

--- compat.py
+++ compat.py
@@ -1,57 +1,29 @@
-# MySQL Connector/Python - MySQL driver written in Python.
-# Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
-
-# MySQL Connector/Python is licensed under the terms of the GPLv2
-# <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most
-# MySQL Connectors. There are special exceptions to the terms and
-# conditions of the GPLv2 as it is applied to this software, see the
-# FOSS License Exception
-# <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-
-"""This module handles compatibility issues between Python 2 and Python 3."""
-
-import sys
-import decimal
-import binascii
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
 
 
-PY3 = sys.version_info.major == 3
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
 
-if PY3:
-    from urllib.parse import urlparse, unquote, parse_qsl
+# Handle the case where the requests module has been patched to not have
+# urllib3 bundled as part of its source.
+try:
+    from pip._vendor.requests.packages.urllib3.response import HTTPResponse
+except ImportError:
+    from pip._vendor.urllib3.response import HTTPResponse
 
-    def hexlify(data):
-        return binascii.hexlify(data).decode("utf-8")
+try:
+    from pip._vendor.requests.packages.urllib3.util import is_fp_closed
+except ImportError:
+    from pip._vendor.urllib3.util import is_fp_closed
 
-    NUMERIC_TYPES = (int, float, decimal.Decimal,)
-    INT_TYPES = (int,)
-    UNICODE_TYPES = (str,)
-    STRING_TYPES = (str,)
-    BYTE_TYPES = (bytearray, bytes,)
-
-
-else:
-    from urlparse import urlparse, unquote, parse_qsl
-
-    def hexlify(data):
-        return data.encode("hex")
-
-    NUMERIC_TYPES = (int, float, decimal.Decimal, long,)
-    INT_TYPES = (int, long,)
-    UNICODE_TYPES = (unicode,)
-    STRING_TYPES = (str, unicode,)
-    BYTE_TYPES = (bytearray,)
+# Replicate some six behaviour
+try:
+    text_type = unicode
+except NameError:
+    text_type = str
