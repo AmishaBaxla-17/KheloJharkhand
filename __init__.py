fa0104c40f5161650c9762d4ef980b9b8bb93ebc
AmishaBaxla-17 <92683278+AmishaBaxla-17@users.noreply.github.com>
2022-07-17 20:00:15 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,23 +1,6 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2012-2019 Vinay Sajip.
-# Licensed to the Python Software Foundation under a contributor agreement.
-# See LICENSE.txt and CONTRIBUTORS.txt.
-#
-import logging
+"""Modules copied from Python 3 standard libraries, for internal use only.
 
-__version__ = '0.3.1'
-
-class DistlibException(Exception):
-    pass
-
-try:
-    from logging import NullHandler
-except ImportError: # pragma: no cover
-    class NullHandler(logging.Handler):
-        def handle(self, record): pass
-        def emit(self, record): pass
-        def createLock(self): self.lock = None
-
-logger = logging.getLogger(__name__)
-logger.addHandler(NullHandler())
+Individual classes and functions are found in d2._backport.misc.  Intended
+usage is to always import things missing from 3.1 from that module: the
+built-in/stdlib objects will be used if found.
+"""
